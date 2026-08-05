644a5957ad00ba0e3d06e6d97d3bd428626cb836
Muhammadhussein Ammari <xmha97@gmail.com>
2026-08-06 00:54:23 +0330

    Update

--- Install (ImageGlass).bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-COPY ".\Ext-Icons" "%LocalAppData%\ImageGlass\Ext-Icons"
-ECHO Done, press any key to exit . . .
-PAUSE>NUL
--- Install (PhotoQt).bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-COPY ".\filetypes" "C:\Program Files\PhotoQt\filetypes"
-ECHO Done, press any key to exit . . .
-PAUSE>NUL
